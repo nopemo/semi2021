e102fa53cab95386766b040bcb4956c6dbff6a53
nopemo_ubuntu <mukoyama-kei969@g.ecc.u-tokyo.ac.jp>
2021-12-15 14:45:28 +0900

    12/15の進捗

--- semi2021_robot/scripts/control_tello.py
+++ semi2021_robot/scripts/control_tello.py
@@ -34,13 +34,16 @@ class new_Point2D(Point2D):
 
 class check_node():
     def __init__(self):
-
         self.point_blue=[new_Point2D(0,0),False]
         self.point_green=[new_Point2D(0,-1),False]
         self.dist=60
+        self.face_width=200
+        self.max_flip_dist=100
         self.face=[new_Point2D(0,0),False]
         self.camera_width=640
         self.camera_height=480
+
+        self.flip_or_slide=True
 
         self.sub_blue = rospy.Subscriber('blue_conter/rectangles', RotatedRectArrayStamped, self.callback_blue)
         self.sub_green = rospy.Subscriber('green_conter/rectangles', RotatedRectArrayStamped, self.callback_green)
@@ -62,22 +65,42 @@ class check_node():
     def callback_face(self,msg):
         if len(msg.rects)==1:
             rospy.loginfo("callback_face")
-            self.face=[msg[0],True]
+            rospy.loginfo(msg.rects)
+            self.face=[msg,True]
             
     def calc_vel(self):
         if self.face[1]==True:
+            self.face[1]=False
             tmp_twist=Twist()
-            tmp_twist.linear.y=(-msg.rects[0].width+self.dist)/70
-            tmp_twist.angular.z=(-(msg.rects[0].x+msg.rects[0].width/2.0)+self.camera_width/2.0)/70.0
+            if self.face[0].rects[0].width>self.face_width:
+                tmp_twist.linear.x=-0.4
+            else:
+                tmp_twist.linear.x=0.4
+            # tmp_twist.linear.x=(-self.face[0].rects[0].width+self.face_width) /500.0
+            tmp_twist.angular.z=(-(self.face[0].rects[0].x+self.face[0].rects[0].width/2.0)+self.camera_width/2.0)/500.0
             rospy.loginfo("calc_vel")
             rospy.loginfo(tmp_twist)
             self.pub_vel.publish(tmp_twist)
+
     def calc_distance(self):
         if self.point_blue[1] and self.point_green[1]:
             self.point_green[1]=False
             self.point_blue[1]=False
             rospy.loginfo("calc_distance")
             self.dist=sqrt((self.point_blue[0]-self.point_green[0])*(self.point_blue[0]-self.point_green[0]))
+            if self.dist<self.max_flip_dist:
+                if self.flip_or_slide:
+                    self.pub_flip.publish(3)
+                    rospy.loginfo("flip3")
+                else:
+                    tmp_twist=Twist()
+                    tmp_plusminus=((-self.point_green[0].x-self.camera_width/2.0)!=0)*(((-self.point_green[0].x-self.camera_width/2.0)>0)*2-1)
+                    tmp_twist.angular.z=0.5*tmp_plusminus
+                    tmp_twist.linear.y=0.5*tmp_plusminus
+                    rospy.loginfo("slide 1sec")
+                    self.pub_vel.publish(tmp_twist)
+                    rospy.sleep(1)
+                    rospy.loginfo("slide end")
 
     def run(self):
         while not rospy.is_shutdown():
